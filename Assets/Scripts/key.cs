6d2c4ad0e1f12a8a48a5031d8caebabcbb1804eb
soci222g <116338784+soci222g@users.noreply.github.com>
2023-02-18 19:56:45 +0100

    key i door open fets

--- Assets/Scripts/key.cs
+++ Assets/Scripts/key.cs
@@ -6,28 +6,19 @@ using UnityEngine.Events;
 public class key : MonoBehaviour
 {
 
-    public Collider2D cl;
+
     [SerializeField]
     private UnityEvent collisonEneter;
-    [SerializeField]
-    private UnityEvent collisonExit;
-    void Start()
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
     {
-       cl = GetComponent<Collider2D>();
-    }
-        private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (cl.transform.tag == "player")
+  
+        if (collision.gameObject.tag == "player")
         {
             collisonEneter?.Invoke();
         }
     }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (cl.transform.tag == "player")
-        {
-            collisonExit?.Invoke();
-        }
-    }
+
 
 }
